f1d3111520e0b8c7a3c7aba5f3a35dbcdebf0772
mitselis <mitselisss@gmail.com>
2023-01-18 14:21:52 +0200

    uploading the front end folder

---
+++ frontend/src/pages/NotePage.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
+
+const NotePage = ({ match, history }) => {
+  let noteId = match.params.id;
+  let [note, setNote] = useState(null);
+
+  useEffect(() => {
+    getNote();
+  }, [noteId]);
+
+  let getNote = async () => {
+    if (noteId === "new") return;
+
+    let response = await fetch(`/api/notes/${noteId}/`);
+    let data = await response.json();
+    setNote(data);
+  };
+
+  let createNote = async () => {
+    fetch(`/api/notes/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
+  let updateNote = async () => {
+    fetch(`/api/notes/${noteId}/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
+  let deleteNote = async () => {
+    fetch(`/api/notes/${noteId}/`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    history.push("/");
+  };
+
+  let handleSubmit = () => {
+    if (noteId !== "new" && note.body === "") {
+      console.log("delete:", note);
+      deleteNote();
+    } else if (noteId !== "new") {
+      updateNote();
+    } else if (noteId === "new" && note.body !== null) {
+      createNote();
+    }
+    history.push("/");
+  };
+
+  return (
+    <div className="note">
+      <div className="note-header">
+        <h3>
+          <ArrowLeft onClick={handleSubmit} />
+        </h3>
+        {noteId !== "new" ? (
+          <button onClick={deleteNote}>Delete</button>
+        ) : (
+          <button onClick={handleSubmit}>Done</button>
+        )}
+      </div>
+      <textarea
+        onChange={(e) => {
+          setNote({ ...note, body: e.target.value });
+        }}
+        value={note?.body}
+      ></textarea>
+    </div>
+  );
+};
+
+export default NotePage;
